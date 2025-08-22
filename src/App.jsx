1f2dfe66a988f7d6be3f1b536e471b8772962c1a
arshiasabzalipour <arshiaj216@gmail.com>
2025-08-22 16:50:02 +0330

    todo

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {Header} from "./components/Header"
+import {Tabs} from "./components/Tabs"
+import {TodoList} from "./components/TodoList"
+import {TodoInput} from "./components/TodoInput"
+  
+
+function App() {
+  const todos = [
+    {input: 'hello! Add your first todo!', complete: true},
+    {input: 'Get the groceries' , complete: false},
+    {input: 'learn how to web design', complete: false},
+    {input: 'say hi to gran gran' , complete: true}
+  ]
+  return (
+    <>
+      <Header todos={todos} />
+      <Tabs todos={todos} />
+      <TodoList todos={todos} />
+      <TodoInput />
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,24 @@
+export function TodoList(props) {
+  const { todos } = props;
+  const tab = "All";
+  const filterTodoList =
+        tab === "Completed"
+      ? todos
+      : tab === "Completed"
+      ? todos.filter((val) => val.complete)
+      : todos.filter((val) => !val.complete);
+
+  return (
+    <div>
+      {filterTodoList.map((todo, todoIndex) => {
+        return (
+          <TodoCard 
+            key={todoIndex} 
+            todoIndex={todoIndex} 
+            todo={todo} 
+          />
+        );
+      })}
+    </div>
+  );
+}
